2408bbadcb1e78cf3750f49e349847ae7a0d467b
BondarenkoMM <masya.klas3000@gmail.com>
2024-04-23 09:38:13 +0200

    Updated classes: LoggingAspect, logback.log/ logback-spring.xml
    other: -
    
    Created: -
    
    Deleted: -
    
    Description: added aspect
    
    General: -

--- src/main/java/com/example/ticketmarket_pet/generator/UserDataGenerator.java
+++ src/main/java/com/example/ticketmarket_pet/generator/UserDataGenerator.java
@@ -1,12 +1,12 @@
-package com.example.ticketmarket_pet.generator;
+//package com.example.ticketmarket_pet.generator;
 
-import com.github.javafaker.Faker;
-
-public class UserDataGenerator {
-    private static final Faker FAKER = new Faker();
-    public static String genPhoneNumber(){
-        return FAKER.phoneNumber().toString();
-    }
-
-
-}
+//import com.github.javafaker.Faker;
+//
+//public class UserDataGenerator {
+//    private static final Faker FAKER = new Faker();
+//    public static String genPhoneNumber(){
+//        return FAKER.phoneNumber().toString();
+//    }
+//
+//
+//}
